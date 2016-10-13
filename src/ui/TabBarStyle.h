b55efdba6da2010700ed0f2d8bfe63523bd5ae04
frederic2ec <freddy.cho@hotmail.com>
2016-10-13 17:47:28 -0400

    Update TabBarStyle.h

--- src/ui/TabBarStyle.h
+++ src/ui/TabBarStyle.h
@@ -1,5 +1,5 @@
 /**************************************************************************
-* Meerkat Browser: Web browser controlled by the user, not vice-versa.
+* Otter Browser: Web browser controlled by the user, not vice-versa.
 * Copyright (C) 2015 - 2016 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
@@ -17,12 +17,12 @@
 *
 **************************************************************************/
 
-#ifndef MEERKAT_TABBARSTYLE_H
-#define MEERKAT_TABBARSTYLE_H
+#ifndef OTTER_TABBARSTYLE_H
+#define OTTER_TABBARSTYLE_H
 
 #include <QtWidgets/QProxyStyle>
 
-namespace Meerkat
+namespace Otter
 {
 
 class TabBarStyle : public QProxyStyle
