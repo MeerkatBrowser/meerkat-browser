e51c4934728c53ec65711e7a1318ba1e1fe90671
frederic2ec <freddy.cho@hotmail.com>
2016-11-22 20:13:23 -0500

    Update KeyboardShortcutDelegate.h

--- src/ui/preferences/KeyboardShortcutDelegate.h
+++ src/ui/preferences/KeyboardShortcutDelegate.h
@@ -1,5 +1,5 @@
 /**************************************************************************
-* Meerkat Browser: Web browser controlled by the user, not vice-versa.
+* Otter Browser: Web browser controlled by the user, not vice-versa.
 * Copyright (C) 2013 - 2016 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
@@ -17,12 +17,12 @@
 *
 **************************************************************************/
 
-#ifndef MEERKAT_KEYBOARDSHORTCUTDELEGATE_H
-#define MEERKAT_KEYBOARDSHORTCUTDELEGATE_H
+#ifndef OTTER_KEYBOARDSHORTCUTDELEGATE_H
+#define OTTER_KEYBOARDSHORTCUTDELEGATE_H
 
 #include <QtWidgets/QItemDelegate>
 
-namespace Meerkat
+namespace Otter
 {
 
 class KeyboardShortcutDelegate : public QItemDelegate
