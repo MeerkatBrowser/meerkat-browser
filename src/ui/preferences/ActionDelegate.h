0916afc2d2143476d479bc60f0ceb28f2c24d915
frederic2ec <freddy.cho@hotmail.com>
2016-11-22 20:09:56 -0500

    Update ActionDelegate.h

--- src/ui/preferences/ActionDelegate.h
+++ src/ui/preferences/ActionDelegate.h
@@ -1,5 +1,5 @@
 /**************************************************************************
-* Meerkat Browser: Web browser controlled by the user, not vice-versa.
+* Otter Browser: Web browser controlled by the user, not vice-versa.
 * Copyright (C) 2015 - 2016 Michal Dutkiewicz aka Emdek <michal@emdek.pl>
 *
 * This program is free software: you can redistribute it and/or modify
@@ -17,13 +17,13 @@
 *
 **************************************************************************/
 
-#ifndef MEERKAT_ACTIONDELEGATE_H
-#define MEERKAT_ACTIONDELEGATE_H
+#ifndef OTTER_ACTIONDELEGATE_H
+#define OTTER_ACTIONDELEGATE_H
 
 #include <QtCore/QObject>
 #include <QtWidgets/QItemDelegate>
 
-namespace Meerkat
+namespace Otter
 {
 
 class ActionDelegate : public QItemDelegate
